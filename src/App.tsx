65f06090076074b5baed3f760baa44b07d094d91
Kelvin Yong <13miles@gmail.com>
2018-07-22 17:58:23 +0800

    Add sample Hello component and allow absolute import

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,14 @@
 
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
-
-const instructions = Platform.select({
-	ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-	android:
-		"Double tap R on your keyboard to reload,\n" +
-		"Shake or press menu button for dev menu",
-});
+import { Hello } from "src/components/Hello";
 
 interface Props {}
 export default class App extends Component<Props> {
 	public render() {
 		return (
 			<View style={styles.container}>
-				<Text style={styles.welcome}>Welcome to React Native!</Text>
-				<Text style={styles.instructions}>
-					To get started, edit App.js
-				</Text>
-				<Text style={styles.instructions}>{instructions}</Text>
+				<Hello name="Person" enthusiasmLevel={5} />
 			</View>
 		);
 	}
@@ -35,15 +25,5 @@ const styles = StyleSheet.create({
 		justifyContent: "center",
 		alignItems: "center",
 		backgroundColor: "#F5FCFF",
-	},
-	welcome: {
-		fontSize: 20,
-		textAlign: "center",
-		margin: 10,
-	},
-	instructions: {
-		textAlign: "center",
-		color: "#333333",
-		marginBottom: 5,
 	},
 });
